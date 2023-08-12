511cb36272a0f035fd1996377038117109cd1ba1
MoonLight <xiaozouzhiqiang@gmail.com>
2023-08-12 12:19:40 +0800

    first commit
    create project

---
+++ app/src/main/java/com/example/reflection/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.reflection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.reflection.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'reflection' library on application startup.
+    static {
+        System.loadLibrary("reflection");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'reflection' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
