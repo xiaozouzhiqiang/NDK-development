0355d3a7582b5e3bb565151533b16bcb6161140d
MoonLight <xiaozouzhiqiang@gmail.com>
2023-08-23 23:56:21 +0800

    JNI API
    NDK 性能和 JNI 中的基础数据类型

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -6,17 +6,37 @@ extern "C"{
 #include "android/log.h"
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_reflection_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
+Java_com_example_reflection_MainActivity_stringFromJNI(JNIEnv* env,jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_com_example_reflection_MainActivity_StaticFun(
-        JNIEnv* env,
-        jclass /* this */) {
+Java_com_example_reflection_MainActivity_StaticFun(JNIEnv* env,jclass /* this */) {
     int result = add_C(4,6);
-    __android_log_print(ANDROID_LOG_INFO, "zouzhiqiang", "test-->addC %d",result);
+    __android_log_print(ANDROID_LOG_INFO, "Moonlight", "test-->addC %d",result);
+}
+
+extern "C" JNIEXPORT jint JNICALL
+Java_com_example_reflection_MainActivity_jni_1add(JNIEnv *env, jobject thiz, jint num) {
+    // TODO: implement Jni_add()
+    int result = 0;
+    for (int i = 0; i < num; ++i) {
+        result = result + i ;
+    }
+    return result;
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_reflection_MainActivity_testStringApi(JNIEnv *env, jobject thiz,jstring content) {
+    // TODO: implement testStringApi()
+    const char *a = env->GetStringUTFChars(content, nullptr);
+    int contentSize = env->GetStringUTFLength(content);
+    if(a != nullptr){
+        __android_log_print(ANDROID_LOG_INFO, "Moonlight", "GetStringUTFChars:%s, Content Size %d", a,contentSize);
+    }
+    //防止内存泄漏问题，一般会将内存进行释放，由于这儿会有返回值，所以不释放内存
+    //env->ReleaseStringUTFChars(content,a);
+    jstring result = env->NewStringUTF("Hello from Jni");
+    return result;
 }
--- app/src/main/java/com/example/reflection/MainActivity.java
+++ app/src/main/java/com/example/reflection/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.reflection;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.example.reflection.databinding.ActivityMainBinding;
@@ -19,20 +20,42 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         // Example of a call to a native method
         TextView tv = binding.sampleText;
         tv.setText(stringFromJNI());
+        execution_time(); //计算执行java层和jni层相同代码所花费的时间
+        testJniAPI(); //测试JNI中的基础数据类型
     }
 
+    public void execution_time(){
+        long start = System.nanoTime();
+        int result = java_add(1000000);
+        long end = System.nanoTime();
+        Log.i("Java_add","from java_add"+ result + "Java层java_add函数执行所花费的时间：" + (end - start));
+        start = System.nanoTime();
+        result = jni_add(1000000);
+        end = System.nanoTime();
+        Log.i("Jni_add","from Jni_add"+ result + "Jni层Jni_add函数执行所花费的时间：" + (end - start));
+    }
+    public int java_add(int num){
+        int result = 0;
+        for (int i=0;i<=num;i++){
+            result = result + i;
+        }
+        return  result;
+    }
+    public void testJniAPI(){
+        String resultString = testStringApi("hello from java");
+        Log.i("Moonlight","resultString" + resultString);
+    }
     /**
      * A native method that is implemented by the 'reflection' native library,
      * which is packaged with this application.
      */
     public native String stringFromJNI();
     public static native void StaticFun();
-
+    public native int jni_add(int num);
+    public native String testStringApi(String content);
 }
